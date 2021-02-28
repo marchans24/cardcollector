ff7d96b5edec68d7014f41c5cfc25bd9b04d3e2d
Marcus Hansen <marcusEhansen@gmail.com>
2021-02-28 00:06:08 -0600

    add update and delete

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Card
 
 
@@ -17,3 +18,15 @@ def cards_index(request):
 def cards_detail(request, card_id):
   card = Card.objects.get(id=card_id)
   return render(request, 'cards/detail.html', { 'card': card })
+
+class CardCreate(CreateView):
+    model = Card
+    fields = '__all__'
+    
+class CardUpdate(UpdateView):
+    model = Card
+    fields = ['make', 'value', 'year']
+
+class CardDelete(DeleteView):
+    model = Card
+    success_url = '/cards/'
